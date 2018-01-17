c6f327c4dce4ecd60bc3bdcc652431bac3b1b406
viddamao <viddamaowj@hotmail.com>
2018-01-16 22:47:52 -0500

    remove unused code

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(serveStatic(__dirname + "/dist"));
 app.use(bodyParser.json({ type: 'application/x-www-form-urlencoded'})); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-//send email once received contact form
+//send email once received contact form post
 app.post('/contactform', function (req, res) {
         let data = req.body
         console.log(req.body)       
@@ -29,7 +29,7 @@ app.post('/contactform', function (req, res) {
         })
         let mailOptions = {
             from: 'viddamaowj@hotmail.com', // sender address
-            to: 'viddamao@gmail.com', // list of receivers
+            to: 'info@newtouchdigital.com', // list of receivers
             subject: 'Contact form', // Subject line
             text: 'Name: ' + data.name+ '\n' 
             +  'Time: ' + data.time + '\n' 
@@ -53,66 +53,3 @@ var port =process.env.PORT || 5000
 app.listen(port);
  
 console.log('server started '+ port);
-
-/*
-var http = require('http');
-http.globalAgent.maxSockets = 10;
-var port = 5000;
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-
-//var server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-// server.listen(port);
-// server.on('error', onError);
-// server.on('listening', onListening);
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-/*
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-/*
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-}
-
-
-*/
